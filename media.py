413bdce7b8803d0be4368af901705b885139aee4
Enver Ablaiev <taraqtash@gmail.com>
2016-07-31 01:26:19 +0300

    indentation fixed

--- media.py
+++ media.py
@@ -1,14 +1,14 @@
 import webbrowser
 
 class Movie(object):
-      '''This class provides a way to store movie related info'''
-      def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-            self.title = movie_title
-            self.story_line = movie_storyline
-            self.poster_image_url = poster_image
-            self.trailer_youtube_url = trailer_youtube
+    '''This class provides a way to store movie related info'''
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):            
+        self.title = movie_title
+        self.story_line = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
-      def show_trailer(self):
-      	"""open a trailer url in a browser"""
-            webbrowser.open(self.trailer_youtube_url)
-            
+    def show_trailer(self):     
+        '''open a trailer url in a browser'''
+        webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.trailer_youtube_url)
